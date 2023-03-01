d6069fac5e59e0e5145f5ed8374fec161c729cc2
Eden <derhyeden@gmail.com>
2023-03-01 10:57:48 +0200

    labelPicker works

--- cmps/labelPicker.js
+++ cmps/labelPicker.js
@@ -1,12 +1,12 @@
 export default {
-  template: `
+	template: `
   <section class="labels">
     <button @click="toggleLabels">Labels</button>
    
   <template v-if="showLabels">
     <input type="search" placeholder="Pick a label" v-model="searchKey"/>
     <ul>
-      <li v-for="label in labels" @click="addLabel(label)">
+      <li v-for="label in labelsToShow" @click="addLabel(label)" :key="label.title">
         {{label.title}}
         {{label.isSelected}}
         
@@ -18,50 +18,50 @@ export default {
   </section>
   `,
 
-  data() {
-    return {
-      labels: [
-        {
-          title: 'important',
-          isSelected: false,
-        },
-        {
-          title: 'family',
-          isSelected: false,
-        },
-        {
-          title: 'work',
-          isSelected: false,
-        },
-        {
-          title: 'friends',
-          isSelected: false,
-        },
-        {
-          title: 'spam',
-          isSelected: false,
-        },
-        {
-          title: 'romantic',
-          isSelected: false,
-        },
-      ],
-      showLabels: false,
-      searchKey: '',
-    }
-  },
-  methods: {
-    addLabel(label) {
-      label.isSelected = !label.isSelected
-    },
-    toggleLabels() {
-      this.showLabels = !this.showLabels
-    },
-  },
-  computed: {
-    labelsToShow() {
-      let regex = new RegExp(this.searchKey, 'i')
-      return this.labels.filter((label) => regex.test(label.title))
-    },
-  },
+	data() {
+		return {
+			labels: [
+				{
+					title: 'important',
+					isSelected: false,
+				},
+				{
+					title: 'family',
+					isSelected: false,
+				},
+				{
+					title: 'work',
+					isSelected: false,
+				},
+				{
+					title: 'friends',
+					isSelected: false,
+				},
+				{
+					title: 'spam',
+					isSelected: false,
+				},
+				{
+					title: 'romantic',
+					isSelected: false,
+				},
+			],
+			showLabels: false,
+			searchKey: '',
+		}
+	},
+	methods: {
+		addLabel(label) {
+			label.isSelected = !label.isSelected
+		},
+		toggleLabels() {
+			this.showLabels = !this.showLabels
+		},
+	},
+	computed: {
+		labelsToShow() {
+			let regex = new RegExp(this.searchKey, 'i')
+			return this.labels.filter(label => regex.test(label.title))
+		},
+	},
 }
