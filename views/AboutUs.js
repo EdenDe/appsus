3f844f786690b74bd6ab51f6b9261a5e9bdcc0e7
TamarShalem93 <stamar46@gmail.com>
2023-03-04 20:46:37 +0200

    demo data mails

--- views/AboutUs.js
+++ views/AboutUs.js
@@ -1,15 +1,15 @@
 export default {
-	template: `
+  template: `
     <section class="about-page">
       <h1>About us</h1>
       <main class="flex"> 
         <div class="card"> 
-          <img src="./assets/img/eden.png"/>
-          <p>Eden</p>
-        </div>
-        <div class="card"> 
           <img src="./assets/img/tamar.png"/>
           <p>Tamar</p>
+        </div>
+        <div class="card"> 
+          <img src="./assets/img/eden.png"/>
+          <p>Eden</p>
         </div>
       </main>
     </section>
