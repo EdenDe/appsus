2fcacabbb53e46022f7c525d7d82aa24c2485270
Eden <derhyeden@gmail.com>
2023-03-04 11:32:33 +0200

    added ham btn

--- cmps/AppHeader.js
+++ cmps/AppHeader.js
@@ -1,13 +1,35 @@
 export default {
-  template: `
-        <header class="app-header full">
-            <router-link to="/" class="fa logo"> AppSus</router-link>  
-            <nav>
-                <router-link to="/">Home</router-link>  
-                <router-link to="/mail">Mail</router-link>
-                <router-link to="/note">Keep</router-link>
-                <router-link to="/about">About</router-link>
-            </nav>
-        </header>
+	template: `
+    <header class="app-header full" :class="{'menu-open':openMenu}">
+      <router-link to="/" class="fa logo">AppSus</router-link>  
+      <nav class="main-menu">
+        <router-link to="/">Home</router-link>  
+        <router-link to="/about">About</router-link>
+        <hr>
+        <router-link to="/mail">Email</router-link>
+        <router-link to="/note">Keep</router-link>
+        <router-link to="/note">Book</router-link>
+      </nav>
+      <button :class="btnIcon" class="btn-ham" @click="openMenu = !openMenu"></button>
+      <div class="screen" @click="openMenu = false"></div>
+    </header>
     `,
+	data() {
+		return {
+			openMenu: false,
+		}
+	},
+	watch: {
+		route() {
+			this.openMenu = false
+		},
+	},
+	computed: {
+		btnIcon() {
+			return this.openMenu ? 'fa close' : 'fa bars'
+		},
+		route() {
+			return this.$route
+		},
+	},
 }
--- views/HomePage.js
+++ views/HomePage.js
@@ -1,31 +1,30 @@
 export default {
-  template: `
-        
-            <div class="hero full">
-        	    <h1>Relax <span>We will take care of your's <span class="dynamic">{{wordToShow}}</span></span></h1>
-            </div>             
+	template: `        
+    <div class="home-page full">
+      <h1>Relax <span>We will take care of your's <span class="dynamic">{{wordToShow}}</span></span></h1>
+    </div>             
     `,
-  data() {
-    return {
-      words: ['emails', 'notes', 'books'],
-      word: 'emails',
-      counter: 1,
-      intervalId: 0,
-    }
-  },
-  created() {
-    this.intervalId = setInterval(() => {
-      if (this.counter === 3) this.counter = 0
-      this.word = this.words[this.counter]
-      this.counter++
-    }, 3000)
-  },
-  computed: {
-    wordToShow() {
-      return this.word
-    },
-  },
-  unmounted() {
-    this.intervalId = clearInterval()
-  },
+	data() {
+		return {
+			words: ['emails', 'notes', 'books'],
+			word: 'emails',
+			counter: 1,
+			intervalId: 0,
+		}
+	},
+	created() {
+		this.intervalId = setInterval(() => {
+			if (this.counter === 3) this.counter = 0
+			this.word = this.words[this.counter]
+			this.counter++
+		}, 3000)
+	},
+	computed: {
+		wordToShow() {
+			return this.word
+		},
+	},
+	unmounted() {
+		this.intervalId = clearInterval()
+	},
 }
