1ea3d7f90ed815702675262d013cb5b18e731638
TamarShalem93 <stamar46@gmail.com>
2023-03-04 19:03:34 +0200

    home page

--- views/HomePage.js
+++ views/HomePage.js
@@ -1,13 +1,19 @@
 export default {
   template: `        
     <div class="home-page full">
-      <h1>Relax <span>We will take care of your's <span class="dynamic animate__animated animate__fadeIn">{{wordToShow}}</span></span></h1>
+      <h1>Relax 
+        <span>We will take care of your 
+        <RouterLink :to="link" class="dynamic">{{wordToShow}}</RouterLink>
+        </span>
+      </h1>
     </div>             
     `,
   data() {
     return {
       words: ['emails', 'notes', 'books'],
       word: 'emails',
+      links: ['/mail', '/note', '/book'],
+      link: '/mail',
       counter: 1,
       intervalId: 0,
     }
@@ -16,6 +22,7 @@ export default {
     this.intervalId = setInterval(() => {
       if (this.counter === 3) this.counter = 0
       this.word = this.words[this.counter]
+      this.link = this.links[this.counter]
       this.counter++
     }, 3000)
   },
